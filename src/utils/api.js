ae81b669fcaa2172693dee5ffe2dadf223a8ba2c
arpstha <shrestharp88@gmail.com>
2024-03-12 15:25:04 +0000

    view list of comments from an article

--- src/utils/api.js
+++ src/utils/api.js
@@ -14,4 +14,11 @@ export const getArticlesByID = (article_id) => {
     })
 }
 
+export const getCommentsByarticleId = (article_id) => {
+    return axios.get(`https://nc-news-6vx3.onrender.com/api/articles/${article_id}/comments`)
+    .then((response)=>{
+        return response.data
+    })
+
+}
 
