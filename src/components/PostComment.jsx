0b709876b3337ad1d581b4f639ee1ca174044694
arpstha <shrestharp88@gmail.com>
2024-03-14 11:27:55 +0000

    added post loading and post successful message

--- src/components/PostComment.jsx
+++ src/components/PostComment.jsx
@@ -6,16 +6,19 @@ const PostComment = (params) => {
     const { commentList, setCommentList, article_id } = params
     const {user, setUser} = useContext(UserContext)
     const [isLoading, setIsLoading] = useState(false)
+    
    
     const[newCommnet, setNewCommnet] = useState('')
     const[postError, setPostError] = useState(false)
     const[emptyComment, setEmptyComment] = useState(false)
+    const[postSuccessful, setPostSuccessful] = useState(false)
+    const[postLoading, setPostLoading] = useState(false)
  
     const handlePost = (event) => {
-        console.log(newCommnet,"comments before post")
         event.preventDefault()
-     
+        
         if(newCommnet.length !==0){
+            setPostLoading(true)
             postCommentByArticle_id(article_id, user, newCommnet)
             .then((response)=>{
                 setCommentList((curr)=>{
@@ -23,8 +26,11 @@ const PostComment = (params) => {
                 
                 })
                 setNewCommnet('')
+                setPostLoading(false)
+                setPostSuccessful(true)
             })
             .catch((error)=>{
+                setPostLoading(false)
                 setPostError(true)
             })
 
@@ -45,12 +51,15 @@ const PostComment = (params) => {
         <textarea style={{ width: '350px', height: '50px' }}  id="newCommnet" multiline="true" value={newCommnet} 
         onChange={(event)=>{
             setEmptyComment(false)
+            setPostSuccessful(false)
             setNewCommnet(event.target.value)} }></textarea>
         <br></br>
         <button type='submit'>Post</button>
         <br/>
+        {postLoading? <h5 >Posting... comment please wait.</h5>: null }
         {emptyComment? <h5 className="error">Your comment is empty!</h5>: null }
         {postError? <h5 className="error">Opp!!! Something went wrong during posting your comment!</h5>: null }
+        {postSuccessful? <h5 >Your comment has been posted successfully.</h5>: null }
     </form>
     </div>
 
