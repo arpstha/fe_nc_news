54900151b4f1ffebdc71c3fbd3033a6d639abed7
arpstha <shrestharp88@gmail.com>
2024-03-15 14:20:55 +0000

    task 11 sort articles completed with 1 bug

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -8,14 +8,45 @@ const TopicCard = () => {
 
     const [articleList, setArticleList] = useState([])
     const [isLoading, setIsLoading] = useState(true)
-
+    const [sortBy, setSortBy] = useState('date');
+    const [desc, setDesc] = useState(true)
+    
+    const handleSortChange = (event) => {
+        const selectedValue = event.target.value;
+        setSortBy(selectedValue);
+    }
+    const toggleOrder = () => {
+        setDesc(!desc)
+    }
     useEffect(()=>{
+        setIsLoading(true)
         getAllArticles(topic)
         .then((data)=>{
+            data.forEach((article) => {
+                //re-formating data and time 
+                return article.created_at = (`${article.created_at.slice(0,10)} ${article.created_at.slice(11,16)}`) 
+            })
             setArticleList(data);
             setIsLoading(false);
         })
     },[])
+
+    useEffect(()=>{
+        let sortedList = [...articleList]; 
+        if (sortBy === "created_at") {
+          desc? sortedList.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)) 
+          : sortedList.sort((a, b) => new Date(a.created_at) - new Date(b.created_at));
+        }
+        if (sortBy === "comment_count") {
+          desc? sortedList.sort((a, b) => b.comment_count - a.comment_count) 
+          : sortedList.sort((a, b) => a.comment_count - b.comment_count);
+        }
+        if (sortBy === "votes") {
+          desc? sortedList.sort((a, b) => b.votes - a.votes) 
+          : sortedList.sort((a, b) => a.votes - b.votes);
+        }
+        setArticleList(sortedList);
+    },[sortBy,desc])
  
     return  isLoading ? (
         <div className="loading">
@@ -23,7 +54,19 @@ const TopicCard = () => {
         </div>
     ) : (
         <div className="article-list-container">
-            <h2>Recent articles:</h2>
+            <h2>Articles on {topic}:</h2>
+
+            <label htmlFor="sort-dropdown">Sort article by:</label> 
+            <select name="sort-article" id="sort-article" onChange={handleSortChange}> 
+                <option value="created_at">Date</option>
+                <option value="comment_count">Comment Count</option> 
+                <option value="votes">Votes</option> 
+            </select>
+            <label htmlFor="sort-lable">Article order:</label>
+            <button onClick={toggleOrder}>
+                {desc? 'Decending' : 'Acending'}
+                </button> 
+
             {articleList.map((article, index) => (
                 <div className="article-box" key={index}>
                     <Link to={`/article/${article.article_id}`}>
@@ -34,8 +77,7 @@ const TopicCard = () => {
                         <p>Topic: {article.topic}</p>
                         <p>Votes: {article.votes}</p>
                         <p>Comments Count: {article.comment_count}</p>
-                        
-                        
+                        <p>Created at: {article.created_at}</p>       
                     </div>
                     <img src={article.article_img_url} alt="Article" />
                 </div>
